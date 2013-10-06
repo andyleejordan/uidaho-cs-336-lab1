db0c5e4c8a2fd07ccc1e427365ee13ec261483c5
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2013-10-06 13:16:42 -0700

    Commenting up stack.c

--- stack.c
+++ stack.c
@@ -1,4 +1,4 @@
-/* stack_new.c */
+/* stack.c */
 
 /* This program has a buffer overflow vulnerability. */
 /* Our task is to exploit this vulnerability */
@@ -8,11 +8,31 @@
 
 int bof(char *str)
 {
+    /* buffer here is allocated 12 bytes of memory by the compiler */
     char buffer[12];
+
+    /* int x is our own insertion that we use to print out the address
+       it is given. this is not necessary but was done for
+       informational purposes */
     int x = 0;
-    /* The following statement has a buffer overflow problem */ 
+
+    /* The following statement has a buffer overflow problem */
+    /* strcpy is a vulnerable function because it does not do any
+       bounds checking, insted it will copy the entire second argument
+       into the address space of the first argument, overwriting
+       anything past the first argument's allocated memory. */
     strcpy(buffer, str);
+
+    /* using the allocated int x above, we print the address it was given */
     printf("x: 0x%x\n", x);
+
+    /* This return pointer is allocated in the stack, 16 bytes past
+       the beginning of buffer (plus another 4 bytes with the addition
+       of int x), and can therefore be overwritten.  Since it is a
+       return pointer, at the end of bof, this program will transfer
+       control back to the address pointed by return. If overwritten,
+       we can abuse this to transfer control to wherever we would
+       like: say our shellcode (or the nop sled before it). */
     return 1;
 }
 
@@ -20,9 +40,15 @@ int main(int argc, char **argv)
 {
 
     char str[517];
-    int x;
+
+    /* This program grabs unsanitized input from a file called
+       badfile, which we can fill with exploit code */
     FILE *badfile; 
     badfile = fopen("badfile", "r");
+    /* Notice that it reads 517 bytes of badfile into str, and sends
+       str to bof, which copies it into buffer, which is only 12 bytes
+       long. strcpy doesn't care and will continue past the buffer's
+       bounds */
     fread(str, sizeof(char), 517, badfile);
     bof(str);
 
